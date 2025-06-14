4355d397d0894e1326a641d56443a595b0d80471
yeet420x <ballst69420@gmail.com>
2025-06-14 21:04:04 +0100

    Initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,17 +10,16 @@ const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		adapter: adapter({
-			// default options are shown
-			out: 'build',
-			precompress: false,
-			envPrefix: '',
-			polyfill: true,
-			env: {
-				host: '0.0.0.0',
-				port: process.env.PORT || 8080
-			}
-		})
+		adapter: adapter(),
+		// default options are shown
+		out: 'build',
+		precompress: false,
+		envPrefix: '',
+		polyfill: true,
+		env: {
+			host: '0.0.0.0',
+			port: process.env.PORT || 8080
+		}
 	}
 };
 
